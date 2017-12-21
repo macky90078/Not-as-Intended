b370cee212709119988ab255979d7dce1d9b8116
macky90078 <mackenzie.w115@gmail.com>
2017-12-21 10:49:52 -0500

    First commit

---
+++ Assets/PlayerShapeCreation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShapeCreation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void CreateShapePeices()
+    {
+        if(Input.GetAxis("LVertical") > 0f)
+        {
+            //transform.gameObject
+        }
+        else if(Input.GetAxis("LHorizontal") > 0f)
+        {
+
+        }
+            
+    }
+}
